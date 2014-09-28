aa62adb55d7db4b44bd3e1794ebec7fb2d907902
Afshin Mokhtari <afshinator@hotmail.com>
2014-09-28 13:47:08 -0700

    add mousewheel control

--- t1.js
+++ t1.js
@@ -2,33 +2,57 @@ $(function() {
 
 	var view = function() {
 		var
-			$content = $('.lineviewer-content'),
-			$up = $('.lineviewer-up'),		
-			$down = $('.lineviewer-down');
+			$content = $('.lineviewer-content'),			// main content area
+			$up = $('.lineviewer-up'),						// scroll up button
+			$down = $('.lineviewer-down');					// scroll down button
 
 		var
-			init = function() {},
+			init = function() {},							// TODO:   not necessary now, but as more code is added...
 
-			clear = function() { $content.empty(); },
+			clear = function() { $content.empty(); },							// Clear out main content area 
 			getText = function() { return $content.text(); },
 			getFontSize = function() { return $content.css('font-size'); },
 			height = function() { return $content.height(); },
 
 			captureBrowserResize = function( callback ) { $(window).on( 'resize', callback ); },
 
-			captureScroll = function ( upOrDown, callback ) {
-				// http://stackoverflow.com/questions/4080497/how-can-i-listen-for-a-click-and-hold-in-jquery
+			captureScrollButtons = function ( upOrDown, callback ) {
 				var timeoutId = 0,
 					$el = ( upOrDown === 'up' ) ? $up : $down;
 
-				$el.mousedown(function() {
-				    callback();
+				$el.on( 'mouseover mouseout click', function(e) {
+					if ( e.type === 'mouseover' ) {
+						$(this).addClass('highlight1');
+					} 
+					else if ( e.type === 'mouseout' ) {
+						$(this).removeClass('highlight1');
+					}
+					else if ( e.type === 'click' ) {
+						$(this).hide().fadeIn( 75, function() { callback(); });	// Turn button off & on for visual feedback, as well as call handler
+					}
+				});
+
+				captureMouseWheel( upOrDown, callback );
+			},
+
+
+			captureMouseWheel = function( upOrDown, callback ) {
+				$(window).bind( 'mousewheel', function(e) {
+				    if( upOrDown === 'up' && e.originalEvent.wheelDelta > 0 ) {
+				    	console.log( upOrDown ); 
+				        callback();
+				    } 
+				    if ( upOrDown === 'down' && e.originalEvent.wheelDelta < 0) {
+				    	console.log( upOrDown ); 
+				    	callback();
+				    }
 				});
 			},
 
+
 			render = function( lines, top, linesToShow ) {
 				var html = '', i;
-				console.log('top: '+ top + '   lineToShow :' + linesToShow );
+				// console.log('top: '+ top + '   lineToShow :' + linesToShow );
 				for ( i = 0; i < linesToShow; i++ ) {
 					html += '<span class="line">' + ( i + top ) + ': ' + lines[ top + i ] + '<br></span>';
 				}
@@ -38,12 +62,13 @@ $(function() {
 
 		return {
 			init : init,
-			clear : clear,									// empty the div
-			getText : getText,								// return all the text in the div
+			clear : clear,									// Empty the main content area
+			getText : getText,								// Return all the text in the main content area
 			getFontSize : getFontSize,						// return font-size
 			viewableContentHeight : height,					// return height of the div
 			captureBrowserResize : captureBrowserResize,	// pass in function to execute on browser resize
-			captureScroll : captureScroll,					// 1st param: 'up' or 'down', 2nd: function to execute on scroll
+			captureScrollButtons : captureScrollButtons,	// 1st param: 'up' or 'down', 2nd: function to execute on scroll
+			captureMouseWheel : captureMouseWheel,			// 
 			render : render									// update the DOM;
 		};
 		
@@ -52,10 +77,10 @@ $(function() {
 
 	var model = function() {
 		var
-			originalTxt,
-			lines = [],
-			linesToShow,
-			top;
+			originalTxt,						// Will hold all the original text in the main content div
+			lines = [],							// The original text split up into lines, based on \n
+			linesToShow,						// How many lines there are to show
+			top;								// Current line # being shown at top of main content area
 
 		var
 			init = function( txt ) {
@@ -99,38 +124,38 @@ $(function() {
 
 
 	var controller = function() {
-		var 
-			// Returns a function that ignores all further invocations until wait amount
-			// of time has passed, at which time it will invoke the function once.
-			// Used to limit how many times a callback function is called for gui events.
-			debounce = function(func, wait) {
-				var timeout;
-				return function() {
-					var context = this, args = arguments;
-					var later = function() {
-						timeout = null;
-						func.apply(context, args);
-					};
-					clearTimeout(timeout);
-					timeout = setTimeout(later, wait);
+		// Returns a function that ignores all further invocations until wait amount
+		// of time has passed, at which time it will invoke the function once.
+		// Used to limit how many times a callback function is called for gui events.
+		var debounce = function(func, wait) {
+			var timeout;
+			return function() {
+				var context = this, args = arguments;
+				var later = function() {
+					timeout = null;
+					func.apply(context, args);
 				};
-				return func;
-			},
+				clearTimeout(timeout);
+				timeout = setTimeout(later, wait);
+			};
+			return func;
+		},
 
-			init = function() {
-				view.init();
-				model.init( view.getText() );			// get all the text in the content div, give it to the model
-				view.captureBrowserResize( debounce( model.computeLinesToShow, 300 ) );	// tell the view to trigger the model method on browser resize
-				view.captureScroll( 'up', model.handleScrollUp );
-				view.captureScroll( 'down', model.handleScrollDown );	
-			}();
+		init = function() {
+			// view.init();										// TODO: Might be useful sometime in the future
+
+			// Initialize model with text in main content area
+			model.init( view.getText() );						
+
+			view.captureBrowserResize( debounce( model.computeLinesToShow, 300 ) );	// Setup view to tell the model when browser resizes
+			view.captureScrollButtons( 'up', model.handleScrollUp );				// Setup view to invoke model methods when scroll buttons pressed
+			view.captureScrollButtons( 'down', model.handleScrollDown );
+		}();
 
 		return {
 			init : init,
 			debounce : debounce
 		}
 	}();	
-
-
 
 });
